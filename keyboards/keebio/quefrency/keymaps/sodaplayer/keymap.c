41dcec9350b8464d04b759107932b3c16f71d970
John Fresco <john.fresco@utah.edu>
2019-08-07 09:41:01 -0600

    update before breating effect

--- keyboards/keebio/quefrency/keymaps/sodaplayer/keymap.c
+++ keyboards/keebio/quefrency/keymaps/sodaplayer/keymap.c
@@ -8,9 +8,14 @@ extern keymap_config_t keymap_config;
 
 #define PS_COM LT(_COM, KC_PAUS)
 
+#define TAPPING_TERM 200
+
 
 
 const uint8_t RGBLED_BREATHING_INTERVALS[] PROGMEM = {200, 100, 50, 25};
+
+static uint16_t numtog_timer;
+static bool numtog_on = false;
 
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
@@ -18,11 +23,17 @@ void keyboard_post_init_user(void) {
     rgblight_sethsv_noeeprom(125, 255, 255);
     // rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
     rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL + 4);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_CHRISTMAS);
 }
 
 // enum custom_keycodes {
 //   QWERTY = SAFE_RANGE,
 // };
+
+enum custom_keycodes {
+    NUMTOG = SAFE_RANGE
+};
+
 
 enum layers {
     _DEF,
@@ -41,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         KC_END,  \
     KC_F3,   KC_F4,   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                   KC_PGUP, \
     KC_F5,   KC_F6,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_INS, KC_PGDN, \
-    KC_F7,   KC_F8,  KC_LCTL, KC_LGUI, KC_LALT, MO(_ARR),KC_SPC,           KC_SPC,  MO(_NUM),KC_RALT, KC_RGUI, KC_MENU, KC_RCTL,                   KC_PSCR, PS_COM
+    KC_F7,   KC_F8,  KC_LCTL, KC_LGUI, KC_LALT, MO(_ARR),KC_SPC,           KC_SPC,  NUMTOG, KC_RALT, KC_RGUI, KC_MENU, KC_RCTL,                   KC_PSCR, PS_COM
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
@@ -105,15 +116,23 @@ void do_layer_color(void) {
             rgblight_set_effect_range(0, 12);
             rgblight_sethsv_range(20, 255, 255, 12, 16);
             break;
-        case(_COM):
-            rgblight_set_effect_range(0, 16);
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
-            rgblight_sethsv_noeeprom(HSV_PURPLE);
+        case(_NUM):
+            rgblight_set_effect_range(0, 11);
+            if (IS_HOST_LED_ON(USB_LED_NUM_LOCK)) {
+                rgblight_sethsv_range(0, 255, 255, 11, 12);
+            } else {
+                rgblight_sethsv_range(0, 125, 255, 11, 12);
+            }
             break;
         case(_PS):
             rgblight_set_effect_range(0, 12);
             rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL + 4);
             rgblight_sethsv_range(125, 255, 255, 12, 16);
+            break;
+        case(_COM):
+            rgblight_set_effect_range(0, 16);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3);
+            rgblight_sethsv_noeeprom(HSV_PURPLE);
             break;
         default:
             rgblight_set_effect_range(0, 16);
@@ -134,3 +153,32 @@ void matrix_scan_user(void) {
     // do_layer_color();
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case NUMTOG:
+            if (record->event.pressed) {
+                numtog_timer = timer_read();               
+                layer_on(_NUM);
+            } else {
+                layer_off(_NUM);
+
+                if (numtog_on) { 
+                    numtog_on = false; 
+                    do_layer_color();
+                    break; 
+                }
+
+                if (timer_elapsed(numtog_timer) < TAPPING_TERM) {
+                    layer_invert(_NUM);
+                    numtog_on = true;
+                }
+            }
+            do_layer_color();
+            break;
+    }
+    return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+    do_layer_color();
+}
